aa57272bcb1677623e6af3e850d8efd5ff6daf68
Steven Senkowski <scsenkowski@gmail.com>
2017-11-13 18:34:56 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,13 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-name_hash = {
-  
+backpacking = {
+towel: 0.5
+tent: 10
+water_filter: .5
+shirts: 2
+pants: 4
+multi_tool: 1
+food: 10
 }
 def key_for_min_value(name_hash)
 

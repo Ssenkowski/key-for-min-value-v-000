403a874fbec98efc8006ef5c67f45997eae2b7d9
Steven Senkowski <scsenkowski@gmail.com>
2017-11-13 18:38:20 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,13 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 backpacking = {
-towel: 0.5
-tent: 10
-water_filter: 1
-shirts: 2
-pants: 4
-multi_tool: 1
-food: 10
+towel: 0.5,
+tent: 10,
+water_filter: 1,
+shirts: 2,
+pants: 4,
+multi_tool: 1,
+food: 10,
 }
 def key_for_min_value(backpacking)
   backpacking.each do |key, value|
